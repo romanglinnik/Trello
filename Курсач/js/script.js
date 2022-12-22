c3741ce4ddb29a3cdbb5060e752d5284bee2b1e3
romanglinnik <55718089+romanglinnik@users.noreply.github.com>
2022-12-22 16:04:16 +0300

    Delete Курсач directory

--- Курсач/js/script.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(){
-    window.setInterval(function(){
-    let now = new Date();
-    let clock = document.getElementById("header__clock");
-    clock.innerHTML = now.toLocaleTimeString().slice(0, -3);
-    },1000);
-  };
