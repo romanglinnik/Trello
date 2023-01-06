b9a6244839e3c28f4ca17b7cf8e195d1e0fbf29f
Alllena <khatkevich.alena@gmail.com>
2023-01-06 13:34:17 +0300

    fixed a branch merge bug

--- src/js/modules/main.js
+++ src/js/modules/main.js
@@ -45,7 +45,6 @@ for (let elem of document.querySelectorAll(".user__list")) {
   elem.addEventListener("dblclick", toggleUserList); // передаем элементам списка функцию, которая прячет список Users
 }
 
-export const app = function () {
   window.onload = function(){
     window.setInterval(function(){
     let now = new Date();
@@ -53,6 +52,7 @@ export const app = function () {
     clock.innerHTML = now.toLocaleTimeString().slice(0, -3);
     },1000);
   };
+
   document.querySelector(".user__select").addEventListener("input", search); // при фокусе "input" происходит вызов функции поиска
   document
     .querySelector(".user__select")
@@ -131,4 +131,3 @@ export const app = function () {
     .querySelector(".radio__status-d")
     .addEventListener("click", checkStatusBoxes); //вызывает функцию изменения статуса "check-boxes"
 };
-}
